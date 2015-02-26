90846c9e30abd5e8652596b84653c11c6c4914bc
rainrambler <wanganyu@outlook.com>
2015-02-26 22:33:01 +0800

    Added some Chinese words

--- src/poemstar/util/DateTimeUtils.java
+++ src/poemstar/util/DateTimeUtils.java
@@ -7,10 +7,14 @@ import java.util.Calendar;
 import java.util.Date;
 
 /**
- *
+ * Date and time utilities
  * @author xinway
  */
 public class DateTimeUtils {
+    /**
+     * Get string description of current time
+     * @return String description of current time
+     */
     public static String getTimeDesc() {
         Date d = Calendar.getInstance().getTime();
         Format formatter = new SimpleDateFormat("yyyy-MM-dd-hh-mm-ss");
